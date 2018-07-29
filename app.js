37aff035804624fac7b50aa1f33cd257165ead66
Sebastian Lundin <Sebastian.Lundin@Knowit.se>
2018-07-29 16:51:24 +0200

    just added scaleing

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-var DSIZE = 1;
+var DSIZE = 2;
+
+var dots = 45000;
 
 var canvas = document.querySelector("canvas");
 var c = canvas.getContext("2d");
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+function calcCanvasWidhtAndHeight(){
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
 
-console.log(canvas);
+window.addEventListener("resize", function(){
+    initDraw();
+});
 
 var point = function(x, y){
     var _this = this;
@@ -14,39 +20,49 @@ var point = function(x, y){
     _this.y = y;
 };
 
-var pointA = new point(canvas.width/2, canvas.height * 1/4);
-var pointB = new point(canvas.width * 3/4, canvas.height * 3/4);
-var pointC = new point(canvas.width/4, canvas.height * 3/4);
-var startingPoint = new point(canvas.width/2, canvas.height * 2/4);
+function initDraw(){
+    calcCanvasWidhtAndHeight();
+    var pointA = new point(canvas.width/2, canvas.height * 1/4);
+    var pointB = new point(canvas.width * 3/4, canvas.height * 3/4);
+    var pointC = new point(canvas.width/4, canvas.height * 3/4);
+    var startingPoint = new point(canvas.width/2, canvas.height * 2/4);
+    
+    c.fillRect(pointA.x, pointA.y, DSIZE, DSIZE);
+    c.fillRect(pointB.x, pointB.y, DSIZE, DSIZE);
+    c.fillRect(pointC.x, pointC.y, DSIZE, DSIZE);   
+    c.fillRect(startingPoint.x, startingPoint.y, DSIZE, DSIZE);
+    
+    var posX = startingPoint.x;
+    var posY = startingPoint.y;
+    
+    function draw(dots){
+        for(var i = 0; i < dots; i++){
+            var rndNr = getRandomInt(1,6);
+            if(rndNr <= 2){
+                posX = posX + ((pointA.x - posX) * 0.5);
+                posY = posY + ((pointA.y - posY) * 0.5);
+                c.fillRect(posX, posY, DSIZE, DSIZE);
+            }
+            if(rndNr > 2 && rndNr <= 4){
+                posX = posX + ((pointB.x - posX) * 0.5);
+                posY = posY + ((pointB.y - posY) * 0.5);
+                c.fillRect(posX, posY, DSIZE, DSIZE);
+            }
+            if(rndNr > 4)
+            {
+                posX = posX + ((pointC.x - posX) * 0.5);
+                posY = posY + ((pointC.y - posY) * 0.5);
+                c.fillRect(posX, posY, DSIZE, DSIZE);
+            }
+        }
+    }
 
-c.fillRect(pointA.x, pointA.y, DSIZE, DSIZE);
-c.fillRect(pointB.x, pointB.y, DSIZE, DSIZE);
-c.fillRect(pointC.x, pointC.y, DSIZE, DSIZE);
-c.fillRect(startingPoint.x, startingPoint.y, DSIZE, DSIZE);
-
-var posX = startingPoint.x;
-var posY = startingPoint.y;
-for(var i = 0; i < 45000; i++){
-    var rndNr = getRandomInt(1,6);
-    console.log(rndNr);
-    if(rndNr <= 2){
-        posX = posX + ((pointA.x - posX) * 0.5);
-        posY = posY + ((pointA.y - posY) * 0.5);
-        c.fillRect(posX, posY, DSIZE, DSIZE);
-    }
-    if(rndNr > 2 && rndNr <= 4){
-        posX = posX + ((pointB.x - posX) * 0.5);
-        posY = posY + ((pointB.y - posY) * 0.5);
-        c.fillRect(posX, posY, DSIZE, DSIZE);
-    }
-    if(rndNr > 4)
-    {
-        posX = posX + ((pointC.x - posX) * 0.5);
-        posY = posY + ((pointC.y - posY) * 0.5);
-        c.fillRect(posX, posY, DSIZE, DSIZE);
-    }
+    draw(dots);
 }
+
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+initDraw();
